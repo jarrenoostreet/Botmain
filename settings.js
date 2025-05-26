ea99629920f95b77099bc95641b25ff311e2bba3
jarrenoostreet <rezaganz633@gmail.com>
2025-05-26 21:50:13 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Lil Von',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "LilVon",
+  botOwner: 'JARE','Fadly',  // Your name
+  ownerNumber: '6282291863574','6282378481831', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
