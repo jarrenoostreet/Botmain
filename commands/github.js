24897e92320f669b36e4a30ae250cdb0be98ca15
jarrenoostreet <rezaganz633@gmail.com>
2025-05-26 22:37:21 +0800

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,11 +1,11 @@
 async function githubCommand(sock, chatId) {
-    const repoInfo = `*🤖 KnightBot MD*
+    const repoInfo = `*🤖 LilVon KB *
 
 *📂 GitHub Repository:*
-https://github.com/mruniquehacker/Knightbot-MD
+https://github.com/jarrenoostreet/Anjay
 
-*📢 Official Channel:*
-https://youtube.com/@mr_unique_hacker
+*📢 Official Owner:*
+wa.me/6282291863574
 
 _Star ⭐ the repository if you like the bot!_`;
 
@@ -17,7 +17,7 @@ _Star ⭐ the repository if you like the bot!_`;
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'LilVon KB',
                     serverMessageId: -1
                 }
             }
